7e17337ecef95288a247243703514063a999079b
wevertonsreis <weverton.sant.reis@gmail.com>
2018-09-30 15:20:17 -0300

    4 - Listando os produtos

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/config/ServletSpringMVC.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/config/ServletSpringMVC.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -19,4 +22,12 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 		return new String[] { "/" };
 	}
 
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		
+		return new Filter[] {characterEncodingFilter};
+	}
+	
 }
--- casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
@@ -1,8 +1,11 @@
 package br.com.casadocodigo.loja.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.dao.ProdutoDAO;
@@ -10,26 +13,31 @@ import br.com.casadocodigo.loja.model.Produto;
 import br.com.casadocodigo.loja.model.TipoPreco;
 
 @Controller
+@RequestMapping("produtos")
 public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDAO produtoDAO;
 	
-	@RequestMapping("/produtos/form")
+	@RequestMapping("/form")
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		return modelAndView;
 	}
 	
-	@RequestMapping("/produtos")
+	@RequestMapping(method = RequestMethod.POST)
 	public String gravar(Produto produto) {
 		produtoDAO.gravar(produto);
-		
-		System.out.println(produto.getTitulo());
-		System.out.println(produto.getDescricao());
-		System.out.println(produto.getPaginas());
 		return "produtos/ok";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView listar() {
+		List<Produto> produtos = produtoDAO.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
 	}
 	
 }
